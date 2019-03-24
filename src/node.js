0e5de830acdb1efd830c8cd628f094c55573bd90
ssashavor <aleksa1601@gmail.com>
2019-03-24 17:08:40 +0300

    constructors

--- src/node.js
+++ src/node.js
@@ -1,18 +1,22 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+	
 	}
 
 	removeChild(node) {
-
+	
 	}
 
 	remove() {
-
+		
 	}
 
 	swapWithParent() {
